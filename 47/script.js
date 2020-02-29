3211bf8367067c970afdd8949eafd0aaf597cd3a
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-02-29 16:27:43 +0530

    chore: slide 47
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

---
+++ 47/script.js
@@ -0,0 +1,17 @@
+let prevInput = "0";
+let calculationOperation = "";
+let currentInput = "0";
+const numbers = document.querySelectorAll(".number");
+const calculatorScreen = document.querySelector(".calculator_screen");
+const updateScreen = (number) => {
+	calculatorScreen.value = number;
+};
+const inputNumber = (number) => {
+	currentInput += number;
+};
+numbers.forEach((number) => {
+	number.addEventListener("click", (event) => {
+		inputNumber(event.target.value);
+		updateScreen(currentInput);
+	});
+});
